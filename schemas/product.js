73da523b26955059cedb70d95b1cc1949115b2c7
lokit007 <it007bk@gmail.com>
2018-03-04 23:13:23 +0700

    Update 20180304

--- schemas/product.js
+++ schemas/product.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var Category = require("./category");
 
 var ProductSchema = new mongoose.Schema({
     name: {
@@ -13,11 +12,25 @@ var ProductSchema = new mongoose.Schema({
 	}
 }, { minimize: false });
 
-ProductSchema.post('save', function(doc) {
-	console.log('%s - %s da luu product', doc._id, doc.category);
+ProductSchema.post('save', function(doc, next) {
 	doc.model('Category').update(
         { _id: doc.category}, 
-        { $push: { products: doc._id } });
+        { $push: { products: doc } },
+        (err, count) => {
+            if(err) console.log(err.message);
+            next();
+        });
+});
+
+ProductSchema.pre('remove', function(next) {
+    console.log('%s has been removed', this._id);
+    this.model('Category').update(
+        { _id: this.category}, 
+        { $pull: { products: {"_id": this._id} } },
+        (err, count) => {
+            if(err) console.log(err.message);
+            next();
+        });
 });
 
 var Product = mongoose.model('Product', ProductSchema);
